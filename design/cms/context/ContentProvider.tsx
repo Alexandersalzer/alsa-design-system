0acc62df60145d356d4ec0bcf1f2872784137745
William <wassinen04@gmail.com>
2025-08-04 16:52:06 +0200

    tog tillbaka tidigare commit

--- design/cms/context/ContentProvider.tsx
+++ design/cms/context/ContentProvider.tsx
@@ -38,9 +38,6 @@ interface ContentContextType {
   getNavbarContent: () => NavbarContent | undefined;
 }
 
-// Create context BEFORE using it
-const ContentContext = createContext<ContentContextType | undefined>(undefined);
-
 interface ContentProviderProps {
   children: ReactNode;
   initialContent?: WebsiteContent | null;
@@ -167,6 +164,8 @@ export function ContentProvider({ children, initialContent = null }: ContentProv
     </ContentContext.Provider>
   );
 }
+
+const ContentContext = createContext<ContentContextType | undefined>(undefined);
 
 export function useContent() {
   const context = useContext(ContentContext);
