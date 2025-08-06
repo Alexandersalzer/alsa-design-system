d1180961eba5758629ed3185933b9feb86807147
William <wassinen04@gmail.com>
2025-08-06 08:02:57 +0200

    use client export in templates index.ts

--- design/system/components/templates/index.ts
+++ design/system/components/templates/index.ts
@@ -1,3 +1,5 @@
+'use client';
+
 export { Footer } from './footers/footer'; 
 export { Navbar } from './navbars/navbar'; 
 export * from './sections'; 
--- design/system/layout/client/ClientComponents.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-// Client-safe exports of template components
-export { Navbar } from '../../components/templates/navbars/navbar';
-export { Footer } from '../../components/templates/footers/footer'; 
