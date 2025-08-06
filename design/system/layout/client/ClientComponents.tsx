875d70daa7350a8c514c2a74af13df9ff6016070
William <wassinen04@gmail.com>
2025-08-06 07:58:59 +0200

    new approach for export of global components

---
+++ design/system/layout/client/ClientComponents.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+// Client-safe exports of template components
+export { Navbar } from '../../components/templates/navbars/navbar';
+export { Footer } from '../../components/templates/footers/footer'; 
