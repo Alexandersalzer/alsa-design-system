e2c36520aa01ce4b06b0e27206f7af763de53b86
Alexandersalzer <alexandersalzer@mac.home>
2025-08-07 15:45:14 +0200

    borttagen onödig komponent

--- design/system/components/primitives/index.ts
+++ design/system/components/primitives/index.ts
@@ -18,7 +18,6 @@ export * from './Stepper';
 export * from './IconButton';
 export * from './NavigationTab';
 export * from './Typography';
-export * from './BrandColorPicker';
 
 
 // Icon needs special handling since it's a default export
