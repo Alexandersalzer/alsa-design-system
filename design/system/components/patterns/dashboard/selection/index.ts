be181c3ae7cd7a554b1937c319187e478e17f416
Alexandersalzer <alexandersalzer@mac.home>
2025-08-08 00:05:05 +0200

    ny visualchoicegroup

--- design/system/components/patterns/dashboard/selection/index.ts
+++ design/system/components/patterns/dashboard/selection/index.ts
@@ -12,3 +12,5 @@ export type { ColorPickerProps, ColorOption } from './ColorPicker';
 
 export { ChoiceGroup } from './ChoiceGroup';
 export type { ChoiceGroupProps, ChoiceOption } from './ChoiceGroup';
+
+export * from './VisualChoiceGroup'
