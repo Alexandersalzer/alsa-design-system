cde9d326eaaf558c18e48892d5ed5b34efcf05eb
Alexandersalzer <alexandersalzer@mac.home>
2025-08-07 14:18:05 +0200

    added brandcolorpicker

---
+++ design/system/components/primitives/BrandColorPicker/index.ts
@@ -0,0 +1,1 @@
+export { BrandColorPicker, type BrandColorPickerProps, type BrandColorOption } from './BrandColorPicker';
--- design/system/components/primitives/index.ts
+++ design/system/components/primitives/index.ts
@@ -18,6 +18,7 @@ export * from './Stepper';
 export * from './IconButton';
 export * from './NavigationTab';
 export * from './Typography';
+export * from './BrandColorPicker';
 
 
 // Icon needs special handling since it's a default export
