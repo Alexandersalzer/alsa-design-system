2d079b0c99402600f817889ae7ff5d3f36315df0
William <wassinen04@gmail.com>
2025-08-08 10:48:47 +0200

    i18n messaging

--- design/cms/messaging/i18n/child/i18nMessaging.ts
+++ design/cms/messaging/i18n/child/i18nMessaging.ts
@@ -12,24 +12,29 @@ interface I18nMessageHandlerParams {
 
 export const createI18nMessageHandlers = (params: I18nMessageHandlerParams): I18nMessageHandlers => {
   const { setSelectedLanguage, isEditingMode = false } = params;
+  console.log('🌐 [CHILD] createI18nMessageHandlers called with params:', params);
 
   return {
     onLanguageUpdate: (languageCode: string) => {
-      console.log('🌐 Received language update in child:', languageCode, 'isEditingMode:', isEditingMode);
+      console.log('🌐 [CHILD] onLanguageUpdate called with:', languageCode, 'isEditingMode:', isEditingMode);
       
       // Validate that the language code is supported
       if (!isSupportedLocale(languageCode)) {
-        console.warn('Unsupported language code received:', languageCode);
+        console.warn('🌐 [CHILD] Unsupported language code received:', languageCode);
         return;
       }
       
       // Update the language picker state if setter is provided
       if (setSelectedLanguage) {
+        console.log('🌐 [CHILD] Calling setSelectedLanguage with:', languageCode);
         setSelectedLanguage(languageCode);
+      } else {
+        console.log('🌐 [CHILD] No setSelectedLanguage function provided');
       }
       
       // Switch to the new locale using the existing locale utility
       // The isEditingMode parameter ensures proper URL handling
+      console.log('🌐 [CHILD] Calling switchLocale with:', languageCode, 'isEditingMode:', isEditingMode);
       switchLocale(languageCode as SupportedLocale, isEditingMode);
     }
   };
@@ -37,25 +42,38 @@ export const createI18nMessageHandlers = (params: I18nMessageHandlerParams): I18
 
 // Hook for setting up i18n message listener
 export const useI18nMessageListener = (handlers: I18nMessageHandlers) => {
+  console.log('🌐 [CHILD] useI18nMessageListener hook called with handlers:', handlers);
+  
   React.useEffect(() => {
+    console.log('🌐 [CHILD] Setting up message listener effect');
+    
     const handleMessage = (event: MessageEvent) => {
+      console.log('🌐 [CHILD] Received postMessage event:', event);
+      console.log('🌐 [CHILD] Event origin:', event.origin);
+      console.log('🌐 [CHILD] Event data:', event.data);
+      
       const { type, languageCode } = event.data;
       
       switch (type) {
         case 'language-update':
+          console.log('🌐 [CHILD] Processing language-update message with languageCode:', languageCode);
           if (languageCode) {
             handlers.onLanguageUpdate(languageCode);
+          } else {
+            console.warn('🌐 [CHILD] language-update message missing languageCode');
           }
           break;
         default:
-          // Ignore other message types
+          console.log('🌐 [CHILD] Ignoring message type:', type);
           break;
       }
     };
 
+    console.log('🌐 [CHILD] Adding message event listener');
     window.addEventListener('message', handleMessage);
     
     return () => {
+      console.log('🌐 [CHILD] Removing message event listener');
       window.removeEventListener('message', handleMessage);
     };
   }, [handlers]);
@@ -63,25 +81,36 @@ export const useI18nMessageListener = (handlers: I18nMessageHandlers) => {
 
 // Setup i18n message listener (alternative to hook for class components)
 export const setupI18nMessageListener = (handlers: I18nMessageHandlers): (() => void) => {
+  console.log('🌐 [CHILD] setupI18nMessageListener called with handlers:', handlers);
+  
   const handleMessage = (event: MessageEvent) => {
+    console.log('🌐 [CHILD] [SETUP] Received postMessage event:', event);
+    console.log('🌐 [CHILD] [SETUP] Event origin:', event.origin);
+    console.log('🌐 [CHILD] [SETUP] Event data:', event.data);
+    
     const { type, languageCode } = event.data;
     
     switch (type) {
       case 'language-update':
+        console.log('🌐 [CHILD] [SETUP] Processing language-update message with languageCode:', languageCode);
         if (languageCode) {
           handlers.onLanguageUpdate(languageCode);
+        } else {
+          console.warn('🌐 [CHILD] [SETUP] language-update message missing languageCode');
         }
         break;
       default:
-        // Ignore other message types
+        console.log('🌐 [CHILD] [SETUP] Ignoring message type:', type);
         break;
     }
   };
 
+  console.log('🌐 [CHILD] [SETUP] Adding message event listener');
   window.addEventListener('message', handleMessage);
   
   // Return cleanup function
   return () => {
+    console.log('🌐 [CHILD] [SETUP] Removing message event listener');
     window.removeEventListener('message', handleMessage);
   };
 }; 
