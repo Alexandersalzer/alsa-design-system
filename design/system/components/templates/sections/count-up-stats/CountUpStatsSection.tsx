c64cfcd37fb16c74c72b962495913325d890a942
William <wassinen04@gmail.com>
2025-08-10 23:17:56 +0200

    stats count up

--- design/system/components/templates/sections/count-up-stats/CountUpStatsSection.tsx
+++ design/system/components/templates/sections/count-up-stats/CountUpStatsSection.tsx
@@ -66,7 +66,7 @@ export const CountUpStatsSection: React.FC<CountUpStatsSectionProps> = ({
   countColor = 'primary',
   
   subtitleVariant = 'body-lg',
-  subtitleWeight = 'regular',
+  subtitleWeight = 'semibold',
   subtitleColor = 'secondary',
   
   // Button defaults
