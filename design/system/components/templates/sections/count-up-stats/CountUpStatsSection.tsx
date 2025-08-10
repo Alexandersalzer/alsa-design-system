e8d125385271ee3c0519f04a2c85e9f40eabdd71
William <wassinen04@gmail.com>
2025-08-10 23:27:27 +0200

    stats count up

--- design/system/components/templates/sections/count-up-stats/CountUpStatsSection.tsx
+++ design/system/components/templates/sections/count-up-stats/CountUpStatsSection.tsx
@@ -58,7 +58,7 @@ export const CountUpStatsSection: React.FC<CountUpStatsSectionProps> = ({
   countDecimals = 0,
   useEasing = true,
   enableScrollTrigger = true, // Re-enable scroll trigger
-  triggerOffset = 500, // Trigger when element is 500px from bottom of viewport
+  triggerOffset = -500, // Trigger when element is -500px from bottom of viewport
   
   // Typography defaults
   countVariant = 'display-xl',
