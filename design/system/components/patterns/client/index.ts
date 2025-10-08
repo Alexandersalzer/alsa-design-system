576a2fc72f63ff5ac9c69df997c2f9deac55f3ec
Slajmon <simonmodin05@gmail.com>
2025-10-08 23:30:13 +0200

    Add PKLHero pattern component for PKL Consulting

--- design/system/components/patterns/client/index.ts
+++ design/system/components/patterns/client/index.ts
@@ -5,6 +5,7 @@ export * from './kj-footer';
 export * from './NotionFooter';
 export * from './FAQ';
 export * from './Hero';
+export * from './PKLHero';
 export * from './ProcessSteps';
 export * from './spinning-banner';
 export * from './CountUpSection';
