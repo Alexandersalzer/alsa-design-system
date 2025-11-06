2627ee532d2a115a2d1284d8fb80b0fffbe2ef26
Alexandersalzer <alexandersalzer@MacBook-Pro-som-tillhor-Alexander.local>
2025-11-06 14:03:59 +0100

    tagit bort kj contact form som var en dåig pattern

--- design/system/patterns/main/kj-contact-form/index.ts
+++
@@ -1,6 +0,0 @@
-// ===============================================
-// blimpify-ui/design/system/patterns/main/kj-contact-form/index.ts
-// ===============================================
-
-export { KJContactForm } from './kj-contact-form';
-export type { KJContactFormProps } from './kj-contact-form';
