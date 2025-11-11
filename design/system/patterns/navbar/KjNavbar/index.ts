bb80bd9a421ff80915b23c5a878940c474fc002c
Alexandersalzer <alexandersalzer@MacBook-Pro-som-tillhor-Alexander.local>
2025-11-12 00:13:45 +0100

    update

--- design/system/patterns/navbar/KjNavbar/index.ts
+++
@@ -1,2 +0,0 @@
-export { KjNavbar, type KjNavbarProps, type NavItem, type NavMenuItem } from './KjNavbar';
-export { KjNavbar as default } from './KjNavbar';
--- design/system/patterns/navbar/NavbarLegacy/index.ts
+++
@@ -1,2 +0,0 @@
-export { Navbar, type NavbarProps, type NavItem } from './NavbarLegacy';
-export { Navbar as default } from './NavbarLegacy';
