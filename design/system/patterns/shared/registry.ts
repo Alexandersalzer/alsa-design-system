f9c8430d8edc678ba8af1feb6201cbc084265307
Alexandersalzer <alexandersalzer@MacBook-Pro-som-tillhor-Alexander.local>
2025-11-14 17:55:50 +0100

    fix(shared): ButtonGroup export/import style to match SectionBody

--- design/system/patterns/shared/registry.ts
+++ design/system/patterns/shared/registry.ts
@@ -1,5 +1,5 @@
 import SectionBody from './sectionBody/SectionBody';
-import { ButtonGroup } from './ButtonGroup/ButtonGroup';
+import ButtonGroup from './ButtonGroup';
 
 /**
  * Shared patterns registry
