bf08e3f92525c93d55ac672f01621ecbff419d60
William <wassinen04@gmail.com>
2025-11-26 23:55:07 +0100

    raderade /ThemeControls

--- design/system/lib/utils.ts
+++ design/system/lib/utils.ts
@@ -1,13 +1,3 @@
 export function cn(...classes: (string | undefined | null | false)[]): string {
   return classes.filter(Boolean).join(' ');
 }
-
-// Alternative if you want to get fancy later with deduplication
-export function mergeClasses(...classes: (string | undefined | null | false)[]): string {
-  return classes
-    .filter(Boolean)
-    .join(' ')
-    .split(' ')
-    .filter((cls, index, arr) => arr.indexOf(cls) === index) // Remove duplicates
-    .join(' ');
-}
--- design/system/patterns/index.ts
+++ design/system/patterns/index.ts
@@ -1,7 +1,6 @@
 export * from './client';
 export * from './dashboard';
 export * from './shared/sectionBody'
-export * from './shared/ThemeControls';
 export * from './forms';
 export * from './gridPatterns';
 export * from './cards';
