8b09f7b85d7f0322f645869e27372e4db1ca991d
William <wassinen04@gmail.com>
2025-11-27 00:20:43 +0100

    tog bort hooks som ej används

--- design/system/hooks/index.ts
+++ design/system/hooks/index.ts
@@ -1,4 +1,2 @@
 // Export all hooks from this directory
 export { useLocaleHref } from './useLocaleHref';
-export { useTheme } from './useTheme';
-export { useThemeManager } from './useThemeManager';
