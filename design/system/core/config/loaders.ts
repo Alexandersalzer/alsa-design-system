cab13dacd9eb84c34bf68be2fcbade20fcf71824
William <wassinen04@gmail.com>
2025-11-28 14:01:10 +0100

    döpt om getDefaultLocale till getPrimaryLocale

--- design/system/core/config/loaders.ts
+++ design/system/core/config/loaders.ts
@@ -1,7 +1,7 @@
 import { loadJsonFile } from "../utils/loaders";
 import type { WebsiteConfig } from "../types/config";
 
-export async function getDefaultLocale(): Promise<string | null> {
+export async function getPrimaryLocale(): Promise<string | null> {
   const config = await loadJsonFile<WebsiteConfig>("config/config.json");
   return config?.localization?.iso_code || null;
 }
