60a3f74fc004393d2b41f2f1a44820729adcc949
William <wassinen04@gmail.com>
2025-12-01 15:16:53 +0100

    raderade förra client-loader

--- design/system/core/design/index.ts
+++ design/system/core/design/index.ts
@@ -1,4 +1,3 @@
 export { buildCssVars, designSnippet } from './snippet';
 export { getDesignConfig } from './loaders';
-export { default as ClientDesignLoader } from './client-loader';
 export type { DesignConfig } from '../types/design';
