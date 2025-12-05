295ca28ea19abdc78883f0854762c24dd4d8791a
Alexander.salzer <136795989+Alexandersalzer@users.noreply.github.com>
2025-12-05 13:56:11 +0100

    command menu

--- design/system/components/overlays/index.ts
+++ design/system/components/overlays/index.ts
@@ -1,5 +1,6 @@
 export * from './Modal';
 export * from './Popover';
 export * from './Menu';
-export * from './Drawer'
+export * from './Drawer';
 export * from './AriaPopover';
+export * from './CommandMenu';
