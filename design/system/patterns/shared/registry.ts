396b238177b137163d2cc9beb4217bb89929b514
William Hassinen <wassinen04@gmail.com>
2026-01-22 02:54:29 +0100

    testing layout system

--- design/system/patterns/shared/registry.ts
+++ design/system/patterns/shared/registry.ts
@@ -1,6 +1,5 @@
 import SectionBody from './sectionBody/SectionBody';
 import SectionHeader from './SectionHeader/SectionHeader';
-import ButtonGroup from './ButtonGroup';
 import { Divider } from '../../components/layout/divider';
 
 /**
@@ -10,6 +9,5 @@ import { Divider } from '../../components/layout/divider';
 export const sharedPatterns: Record<string, React.ComponentType<any>> = {
   sectionBody: SectionBody,
   sectionHeader: SectionHeader,
-  buttonGroup: ButtonGroup,
   divider: Divider,
 };
