b64882f0c24619e50b50ffc01d9d438bcc9a6f35
William Hassinen <wassinen04@gmail.com>
2026-01-26 14:08:28 +0100

    Button export

--- design/system/components/actions/registry.ts
+++ design/system/components/actions/registry.ts
@@ -1,4 +1,4 @@
-import { Button } from './Button'
+import Button from './Button/Button'
 import { TextLink } from './TextLink'
 import { SelectionCard } from './SelectionCard'
 import { SegmentedControl } from './SegmentedControl'
