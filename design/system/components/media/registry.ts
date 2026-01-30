ebfd45a9a3d087b233548069e35db84b843abb15
William Hassinen <wassinen04@gmail.com>
2026-01-30 03:52:31 +0100

    icon components

--- design/system/components/media/registry.ts
+++ design/system/components/media/registry.ts
@@ -2,6 +2,7 @@ import { Image } from './Image/Image';
 import { Video } from './Video/Video';
 import { Avatar } from './Avatar/Avatar';
 import { Icon } from './Icon/Icon';
+import { IconByName } from './IconByName/IconByName';
 import { Logo } from './Logo/Logo';
 import { SquareImageContainer } from './SquareImageContainer/SquareImageContainer';
 import { VideoShowcase } from './VideoShowcase/VideoShowcase';
@@ -11,6 +12,7 @@ export const mediaComponents: Record<string, React.ComponentType<any>> = {
   video: Video,
   avatar: Avatar,
   icon: Icon,
+  iconByName: IconByName,
   logo: Logo,
   squareImageContainer: SquareImageContainer,
   videoShowcase: VideoShowcase,
