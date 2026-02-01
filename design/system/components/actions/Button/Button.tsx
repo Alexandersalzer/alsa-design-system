b90c452aa781deade35ec7d01bc1cebaf4504fa8
William Hassinen <wassinen04@gmail.com>
2026-02-01 23:14:12 +0100

    wrp form

--- design/system/components/actions/Button/Button.tsx
+++ design/system/components/actions/Button/Button.tsx
@@ -15,7 +15,6 @@ import { Component } from '../../frames/component/Component';
 import { useAction } from '../../../core/actions/useAction';
 import type { ActionConfig, NavigationActionConfig, BookingActionConfig } from '../../../core/actions/types';
 import { openCalendlyPopup, buildCalendlyUrl } from '../../../patterns/widgets/CalendlyModal/CalendlyModal';
-import { CheckCircleIcon } from '@heroicons/react/24/outline';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -242,8 +241,7 @@ export const Button = forwardRef<
     if (isSuccess) {
       return (
         <Component componentKey={componentKey}>
-          <div className="flex items-center gap-2 text-green-600">
-            <CheckCircleIcon className="w-5 h-5 flex-shrink-0" />
+          <div className="text-green-600">
             <Label size={getLabelSize(size)} weight={getLabelWeight(size)} as="span">
               {successMessage || 'Skickat!'}
             </Label>
