f6377e44d4104ce1c59fca2c347355c91a77edeb
William Hassinen <wassinen04@gmail.com>
2026-02-02 11:57:55 +0100

    language selector

--- design/system/components/navigation/registry.ts
+++ design/system/components/navigation/registry.ts
@@ -2,6 +2,7 @@ import { Tab, TabGroup } from './Tabs/Tabs';
 import { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
 import { Pagination } from './Pagination/Pagination';
 import { BackButton } from './BackButton/BackButton';
+import { LanguageSelector } from './LanguageSelector/LanguageSelector';
 
 export const navigationComponents: Record<string, React.ComponentType<any>> = {
   tab: Tab,
@@ -9,4 +10,5 @@ export const navigationComponents: Record<string, React.ComponentType<any>> = {
   breadcrumbs: Breadcrumbs,
   pagination: Pagination,
   backButton: BackButton,
+  languageSelector: LanguageSelector,
 };
