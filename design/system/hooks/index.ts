eabcf420b540ea37d62b947c000edf9095d1f239
William Hassinen <wassinen04@gmail.com>
2026-02-12 14:08:02 +0000

    scroll handler

--- design/system/hooks/index.ts
+++ design/system/hooks/index.ts
@@ -3,3 +3,6 @@ export { useHref } from './useHref';
 export { useHashScroll } from './useHashScroll';
 export { useScrollAnimation } from './useScrollAnimation';
 export type { ScrollAnimationConfig, ScrollAnimationState, ScrollAnimationValues } from './useScrollAnimation';
+
+// Export HashScrollHandler component for convenience
+export { HashScrollHandler } from '../components/navigation/HashScrollHandler';
