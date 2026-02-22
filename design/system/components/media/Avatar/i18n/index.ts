06d03ae01e33ac85b39c6deec69273d5996c0402
Alexander.salzer <136795989+Alexandersalzer@users.noreply.github.com>
2026-02-22 10:52:23 +0000

    feat: add Avatar schema with i18n (sv/en) and register in schemaRegistry

---
+++ design/system/components/media/Avatar/i18n/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Avatar component translations
+ */
+
+import { sv } from './sv';
+import { en } from './en';
+import type { TranslationDictionary, ComponentTranslation } from '../../../../core/schemas/i18n/types';
+
+export const avatarTranslations: TranslationDictionary<ComponentTranslation> = {
+  sv,
+  en,
+};
+
+export { sv, en };
