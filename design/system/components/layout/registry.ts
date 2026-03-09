1522b33cf4af445f9b66f628011e5c78632a5f27
Alexander.salzer <136795989+Alexandersalzer@users.noreply.github.com>
2026-03-09 11:12:42 +0000

    horixoantal process rail

--- design/system/components/layout/registry.ts
+++ design/system/components/layout/registry.ts
@@ -15,6 +15,7 @@ import { Bleed } from './bleed/Bleed';
 import { MasonryGrid, MasonryItem } from './MasonryGrid/MasonryGrid';
 import { Overlay } from './Overlay/Overlay';
 import { BentoGrid, BentoItem } from './BentoGrid/BentoGrid';
+import { HorizontalStepRail } from './HorizontalStepRail/HorizontalStepRail';
 
 export const layoutComponents: Record<string, React.ComponentType<any>> = {
   grid: Grid,
@@ -39,4 +40,5 @@ export const layoutComponents: Record<string, React.ComponentType<any>> = {
   masonryItem: MasonryItem,
   bentoGrid: BentoGrid,
   bentoItem: BentoItem,
+  horizontalStepRail: HorizontalStepRail,
 };
